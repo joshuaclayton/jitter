b2d05fd07e66507c3f6de64af9d7113ba9d7b97b
Joshua Clayton <jclayton@fusionary.com>
2009-01-08 14:18:09 -0500

    Updated core to use settings correctly, make method names more intuitive, handle stop/restart of feeding, and initial html test page

--- src/jquery.jitter.core.js
+++ src/jquery.jitter.core.js
@@ -1,21 +1,22 @@
 (function($) {
   $.jitter = function(settings) {
-    var options = $.extend({}, settings, $.jitter.defaults);
+    var options = $.extend({}, $.jitter.defaults, settings);
     options.currentFeed = $.jitter.feeds[options.feed];
     
     // wrapper for all private instance variables
     var jitter = {};
-    
+    jitter.tweets = [];
+
     // public object
     var public = {};
 
     // private methods
-    var buildData = function() {
-      var data = {};
-      if(jitter.sinceID)              { data.since_id = jitter.sinceID; }
-      if(options.currentFeed.performSearch && options.query) { data.q = options.query; }
-      if(options.currentFeed.filteredUsers && options.users.length) { data.q = $.map(options.users, function(item) { return "from:" + item; }).join(" OR "); }
-      return data;
+    var buildRequestParams = function() {
+      var requestParams = {};
+      if(jitter.sinceID) { requestParams.since_id = jitter.sinceID; }
+      if(options.currentFeed.performSearch && options.query) { requestParams.q = options.query; }
+      if(options.currentFeed.filteredUsers && options.users.length) { requestParams.q = $.map(options.users, function(item) { return "from:" + item; }).join(" OR "); }
+      return requestParams;
     };
 
     var buildURL = function(feedItem, urlOptions) {
@@ -23,28 +24,25 @@
       var url = urlOptions.url || feedItem.url;
       if(feedItem.requiresUsername) { url = url.replace(/\{username\}/, urlOptions.username || options.username); }
       if(feedItem.requiresPassword) { url = url.replace(/\{password\}/, urlOptions.password || options.password); }
-      url += "?" + $.param(buildData());
+      url += "?" + $.param(buildRequestParams());
       return url;
     };
 
     // public instance methods
-    public.tweets = function() { return jitter.tweets; };
-
-    public.feedClass = function() {
-      var feedClass = options.currentFeed.name;
-      if(options.currentFeed.requiresUsername)  { feedClass = feedClass.replace(/\{username\}/, options.username); }
-      if(options.currentFeed.performSearch)     { feedClass = feedClass.replace(/\{query\}/, options.query.cssClassify()); }
-      if(options.currentFeed.filteredUsers)     { feedClass = feedClass.replace(/\{groupName\}/, options.groupName.cssClassify()); }
-      return feedClass;
+    var feedClass = function() {
+      var feedClassName = options.currentFeed.name;
+      if(options.currentFeed.requiresUsername)  { feedClassName = feedClassName.replace(/\{username\}/, options.username); }
+      if(options.currentFeed.performSearch)     { feedClassName = feedClassName.replace(/\{query\}/, options.query.cssClassify()); }
+      if(options.currentFeed.filteredUsers)     { feedClassName = feedClassName.replace(/\{groupName\}/, options.groupName.cssClassify()); }
+      return feedClassName;
     };
 
-    public.feedTitle = function() {
-      var feedTitle = options.currentFeed.title;
-
-      if(options.currentFeed.requiresUsername)  { feedTitle = feedTitle.replace(/\{username\}/, options.username); }
-      if(options.currentFeed.performSearch)     { feedTitle = feedTitle.replace(/\{query\}/, options.query); }
-      if(options.currentFeed.filteredUsers)     { feedTitle = feedTitle.replace(/\{groupName\}/, options.groupName); }
-      return feedTitle;
+    var feedTitle = function() {
+      var feedTitleName = options.currentFeed.title;
+      if(options.currentFeed.requiresUsername)  { feedTitleName = feedTitleName.replace(/\{username\}/, options.username); }
+      if(options.currentFeed.performSearch)     { feedTitleName = feedTitleName.replace(/\{query\}/, options.query); }
+      if(options.currentFeed.filteredUsers)     { feedTitleName = feedTitleName.replace(/\{groupName\}/, options.groupName); }
+      return feedTitleName;
     };
 
     var updateTweets = function() {
@@ -53,14 +51,14 @@
         url: buildURL(options.currentFeed),
         dataType: "jsonp",
         success: function(data) {
-          if(data.results) { data = data.results; }                           // set data to data from search results
-          var originalSinceID = jitter.sinceID;                                      // freeze sinceID to see if sinceID was set from a previous request
+          if(data.results) { data = data.results; }                                                 // set data to data from search results
+          var originalSinceID = jitter.sinceID;                                                     // freeze sinceID to see if sinceID was set from a previous request
           var updatingExistingTweets = originalSinceID && jitter.sinceID;
 
           if(options.currentFeed.trackSince === true && data[0]) { jitter.sinceID = data[0].id; }   // set sinceID to the 'newest' tweet in the results
-          if(options.onUpdate && typeof(options.onUpdate) == "function"){ options.onUpdate(data); }  // trigger the onUpdate callback
+          if(options.onUpdate && typeof(options.onUpdate) == "function"){ options.onUpdate(data); } // trigger the onUpdate callback
 
-          if(updatingExistingTweets) { data = data.reverse(); }               // reverse dataset for unshift
+          if(updatingExistingTweets) { data = data.reverse(); }                                     // reverse dataset for unshift
 
           $.each(data, function(index, item) {
             updatingExistingTweets ? jitter.tweets.unshift(item) : jitter.tweets.push(item);
@@ -68,20 +66,30 @@
         }
       });
     };
-
+    
+    // point to internals
+    public.tweets = function() { return jitter.tweets; };
     public.updateTweets = function() { updateTweets(); };
-
+    
+    // make feed info public
+    public.feedClass = feedClass;
+    public.feedTitle = feedTitle;
+    
     // timer setup 
-    jitter.timer = $.timer(1000 * refreshRate /* {refreshRate} seconds (defaults to 60s) */, function(t) {
+    jitter.timer = $.timer(1000 * options.refreshRate, function(t) {
       updateTweets();
     });
 
     public.stop = function() {
       jitter.timer.stop();
     };
+    
+    public.start = function() {
+      jitter.timer.reset(options.refreshRate);
+    }
 
-    public.updateTweets();
+    updateTweets();
 
     return public;
-  }
+  };
 })(jQuery);
--- src/jquery.jitter.defaults.js
+++ src/jquery.jitter.defaults.js
@@ -1,5 +1,8 @@
 (function($) {
   $.jitter.defaults = {
-    refreshRate: 60
+    refreshRate: 60,
+    feed: "search",
+    query: "jquery",
+    onUpdate: function(tweets) { if(tweets[0]) { alert("Newest Tweet:\n" + tweets[0].text); } else { alert("No new tweets, sorry!"); } }
   };
 })(jQuery);
