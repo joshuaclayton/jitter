91ca6017408d65f12695926c93d87d6d584e00f7
Joshua Clayton <jclayton@fusionary.com>
2009-01-09 22:04:02 -0500

    Added defaults spec

---
+++ spec/models/jquery.jitter.defaults_spec.js
@@ -0,0 +1,26 @@
+Screw.Unit(function() {
+  describe("$.jitter.defaults", function() {
+    var defaults;
+    
+    before(function() {
+      defaults = $.jitter.defaults;
+    });
+    
+    it("should have a default refresh rate of 60 seconds", function() {
+      expect(defaults.refreshRate).to(equal, 60);
+    });
+    
+    it("should default to the search feed", function() {
+      expect(defaults.feed).to(equal, "search");
+    });
+    
+    it("should assign a default search of jquery", function() {
+      expect(defaults.query).to(equal, "jquery");
+    });
+    
+    it("should assign an onUpdate callback for new tweets", function() {
+      var defaultCallback = function(tweets) { if(tweets[0]) { alert("Newest Tweet:\n" + tweets[0].text); } else { alert("No new tweets, sorry!"); } };
+      expect(defaults.onUpdate).to(equal, defaultCallback);
+    });
+  });
+});
