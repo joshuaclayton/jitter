3ba3567c8981215d8cb7f18ed3d92b1101dd4cf6
Joshua Clayton <jclayton@fusionary.com>
2009-01-10 12:34:37 -0500

    Added license and readme; swapped out packed version of jQuery with minified one.

--- jquery/jquery.timer.js
+++ jquery/jquery.timer.js
@@ -3,17 +3,17 @@
     interval = interval || 100;
     if (!callback) { return false; }
     var _timer = function (i, c) {
-      var public = {};
-      public.stop = function () { clearInterval(public.id); };
-      public.internalCallback = function () { c(public); };
-      public.reset = function (resetInterval) {
-        if (public.id) { clearInterval(public.id); }
-        resetInterval = resetInterval || public.interval;
-        public.id = setInterval(public.internalCallback, resetInterval);
+      var self = {};
+      self.stop = function () { clearInterval(self.id); };
+      self.internalCallback = function () { c(self); };
+      self.reset = function (resetInterval) {
+        if (self.id) { clearInterval(self.id); }
+        resetInterval = resetInterval || self.interval;
+        self.id = setInterval(self.internalCallback, resetInterval);
       };
-      public.interval = i;
-      public.id = setInterval(public.internalCallback, public.interval);
-      return public;
+      self.interval = i;
+      self.id = setInterval(self.internalCallback, self.interval);
+      return self;
     };
     return _timer(interval, callback);
   };
